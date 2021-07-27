0184f0c3f14aa4e0136f1f3cfb8645b13a060be4
Jorengarenar <dev@joren.ga>
2021-07-27 10:21:06 +0200

    SPDX

--- src/contact.cpp
+++ src/contact.cpp
@@ -1,26 +1,7 @@
-/*  $Id$
-**   _____                 _____
-**  |   __|_ _ ___ ___ ___|_   _|_ _ _ _
-**  |__   | | | . | -_|  _| | | | | |_'_|
-**  |_____|___|  _|___|_|   |_| |___|_,_|
-**            |_|                Portable
-**  Copyright (C) 2005 Ingo Ruhnke <grumbel@gmx.de>
-**
-**  This program is free software; you can redistribute it and/or
-**  modify it under the terms of the GNU General Public License
-**  as published by the Free Software Foundation; either version 2
-**  of the License, or (at your option) any later version.
-**
-**  This program is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**  GNU General Public License for more details.
-**
-**  You should have received a copy of the GNU General Public License
-**  along with this program; if not, write to the Free Software
-**  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-**  02111-1307, USA.
-*/
+/* SPDX-License-Identifier: GPL-2.0-or-later
+ * Copyright (C) 2021 Jorengarenar <dev@joren.ga>
+ * Copyright (C) 2005 Ingo Ruhnke  <grumbel@gmx.de>
+ */
 
 #include "contact.hpp"
 
